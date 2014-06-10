e878493ad04182705104c2d3001dc83def0de7b0
Benoît Suttor <benoit.suttor@imio.be>
2014-06-10 13:45:43 +0200

    Heritage from deterity item

--- src/imio/media/imio_media.py
+++ src/imio/media/imio_media.py
@@ -1,9 +1,34 @@
 # -*- coding: utf-8 -*-
 from zope.interface import implements
-from .interfaces import IMediaLink
+from plone.supermodel import model
+from zope.schema import TextLine, Int
+from . import _
+from plone.dexterity.content import Item
 
 
-class MediaLink(object):
+class IMediaLink(model.Schema):
+    """ Schema for MediaLink template """
+    remoteUrl = TextLine(
+        title=_(u'URL'),
+        description=u'',
+        default=u'http://'
+    )
+
+    maxwidth = Int(
+        title=_(u'Max width'),
+        description=_(u'Like "1024", this value is in pixel'),
+        required=False,
+        default=1024,
+    )
+
+    maxheight = Int(
+        title=_(u'Max height'),
+        description=_(u'Like "768", this value is in pixel'),
+        required=False,
+    )
+
+
+class MediaLink(Item):
     """ """
     implements(IMediaLink)
 
